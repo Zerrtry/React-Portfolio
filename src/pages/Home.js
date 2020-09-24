2790c54466be37a96bdb32fc8747e3e2e05cee7a
Dmitrii Zverev <61390189+Zerrtry@users.noreply.github.com>
2020-09-24 12:35:34 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
             </p><p>
 After 18 years as a lawyer, I decided to try something new. I started to learn web-development even though I had no previous experience in software. I have recently completed an intense 24-week Full-Stack Coding Bootcamp from Northwestern University,. However, science and technology has always interested me. Even in childhood, my favorite magazines were about building scale models and other entertaining topics related to technology. I'm excited to get professionally into programming and want to work, learn and grow in this field.
             </p>
-            <a href="https://docs.google.com/document/d/1DS2DIkUIm5BBt7SLdHHnEHezM81L-nASURQsF62X2Lg/edit?usp=sharing" target="_blank">Resume</a>
+            <a href="https://drive.google.com/file/d/1p2p-1oei5b8_YcPmY7TefQsIb8qhnBms/view?usp=sharing" target="_blank">Resume</a>
           </Col>
         </Row>
       </Container>
