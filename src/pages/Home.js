1eb6396ed259d4775499f53820952a24298fe2fc
Dmitrii Zverev <61390189+Zerrtry@users.noreply.github.com>
2020-10-01 16:58:03 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ function Home() {
             <p>
             I am Dmitrii Zverev, a Full-Stack Web Developer. Welcome to my portfolio page. Please contact me for full-stack web development projects.
             </p><p>
-After 18 years as a lawyer, I decided to try something new. I started to learn web-development even though I had no previous experience in software. I have recently completed an intense 24-week Full-Stack Coding Bootcamp from Northwestern University,. However, science and technology has always interested me. Even in childhood, my favorite magazines were about building scale models and other entertaining topics related to technology. I'm excited to get professionally into programming and want to work, learn and grow in this field.
+After used to work as a lawyer for a time, I decided to try something new. , I decided to try something new. I started to learn web-development even though I had no previous experience in software. I have recently completed an intense 24-week Full-Stack Coding Bootcamp from Northwestern University,. However, science and technology has always interested me. Even in childhood, my favorite magazines were about building scale models and other entertaining topics related to technology. I'm excited to get professionally into programming and want to work, learn and grow in this field.
             </p>
             <a href="https://drive.google.com/file/d/1p2p-1oei5b8_YcPmY7TefQsIb8qhnBms/view?usp=sharing" target="_blank">Resume</a>
           </Col>
